837c4f46d51a28d5c9b3ab91a64f8e6d2d680b55
Jonathan Bruckman <jbruckman86@gmail.com>
2022-11-14 23:41:23 -0500

    more controller / route updates

--- app/controllers/champions_controller.rb
+++ app/controllers/champions_controller.rb
@@ -1,5 +1,18 @@
 class ChampionsController < ApplicationController
 
-    Post '/champions'
+    get '/champions' do
+        champions = Champions.all
+        champions.to_json
+    end
+
+    post '/champions/new' do
+        champion = Champion.create(
+            name: params[:name]
+            spec: params[:spec]
+            wins: params[:wins]
+            image_URL: params[:image_URL]
+            )
+        champion.to_json    
+    end
 
 end
--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -1,6 +1,23 @@
 class HerosController < ApplicationController
 
-    Post '/heros' do
-        
+    post '/heros' do
+        hero = Hero.create(
+            name: params[:name]
+            spec: params[:spec]
+            health: params[:health]
+            strength: params[:strength]
+            agility: params[:agility]
+            intellect: params[:intellect]
+            wins: params[:wins]
+            image_URL: params[:image_URL]
+        )
+        hero.to_json
+    end
+
+    delete '/heros/:id' do
+        hero = Hero.find(params[:id])
+        hero.destory
+    end
+
 
 end
